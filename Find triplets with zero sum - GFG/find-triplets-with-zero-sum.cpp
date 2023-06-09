9d808ff6a823e0e931d5f644000cea1966f13797
amitkumarg769 <115870951+amitkumarg769@users.noreply.github.com>
2023-06-09 16:59:21 +0530

    Added solution - LeetHub

--- Find triplets with zero sum - GFG/find-triplets-with-zero-sum.cpp
+++ Find triplets with zero sum - GFG/find-triplets-with-zero-sum.cpp
@@ -16,7 +16,7 @@ class Solution{
     { 
         //Your code here
         sort(arr,arr+n);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-2;i++){
             int j=i+1;
             int k=n-1;
             
